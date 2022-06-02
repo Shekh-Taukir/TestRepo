d507ce1fd0191063ddeccbc8ba609c5dbed50521
Shekh Taukir <shekhtaukir13@gmail.com>
2022-06-02 08:20:46 +0530

    Update Commit

--- M1.py
+++ M1.py
@@ -1,12 +1,6 @@
-<<<<<<< HEAD
 def d1(a, b):
     print("Result : ", a * b)
 
 
 print("Name : taukir Shekh")
 d1(10, 3)
-=======
-print(10 * 20)
-a, b = map(int, input("enter two no.").split())
-print(a * b)
->>>>>>> fb5cba62c5e2e2ff1a9e60b317ea545b1105ace2
--- README.md
+++ README.md
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 # ReadMe File for Testing purpose
-=======
-# TestRepo
->>>>>>> fb5cba62c5e2e2ff1a9e60b317ea545b1105ace2
